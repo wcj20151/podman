8855531b28ed0844dffaaf38ad6db24b3579f1cd
Bill <wcj20151@gmail.com>
2025-11-21 18:47:11 -0500

    containerfiles

---
+++ have_a_great.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import platform
+
+# Get OS information
+os_info = platform.platform()
+
+# Print OS release info
+print(f"OS Release: {os_info}")
+print("\nHave a great day! 🎉")
